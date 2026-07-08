1e7812853cf5afebaa84b3f02e744f88ede1e736
your full name <patricia86261@gmail.com>
2026-07-07 23:44:37 -0400

    corregido error de sintaxis

---
+++ Downloads/patricia86261-rgb__qa-project_1783039255 (1)/patricia86261-rgb-qa-project-Urban-Grocers-app-es-6d46c1341d4fa6f4429baef469d7592ed64dee04/configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-32d43118-88fc-44cb-97f7-13611b0f9ef2.containerhub.tripleten-services.com"
+CREATE_USER_PATH = "/api/v1/users/"
+KITS_PATH = "/api/v1/kits"
+authToken = "e26e1528-2661-4c64-a375-a30a9c59b262"
+
+
---
+++ Downloads/patricia86261-rgb__qa-project_1783039255 (1)/patricia86261-rgb-qa-project-Urban-Grocers-app-es-6d46c1341d4fa6f4429baef469d7592ed64dee04/data.py
@@ -0,0 +1,13 @@
+header = {
+    "content-type": "application/json",
+    "Authorization": "Bearer e26e1528-2661-4c64-a375-a30a9c59b262"
+}
+
+user_body = {
+    "firstName": "Max",
+    "phone": "+10005553535",
+    "address": "8042 Lancaster Ave.Hamburg, NY"
+
+}
+
+kit_body = {"name": "Mi Kit"}
